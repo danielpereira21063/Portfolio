b4d3bdcbadb6eea1b1f096ff6f97a7be1daf374d
Daniel Sanches <danielsanches6301@gmail.com>
2022-11-20 00:34:42 -0300

    Novo layout

--- Portfolio.WebUI/obj/Release/net6.0/linux-x64/PubTmp/Out/wwwroot/js/main.js
+++ Portfolio.WebUI/obj/Release/net6.0/linux-x64/PubTmp/Out/wwwroot/js/main.js
@@ -1,27 +1,86 @@
-// style
-import '../sass/main.scss';
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-// navbar
-import { Collapse } from 'bootstrap';
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
 
-// scroll spy
-import { ScrollSpy } from 'bootstrap';
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
-// dark/light mode
-import './themes';
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-// typed js
-import './typed';
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
 
-// curtain js
-import './curtain';
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-// from functions
-import './contact';
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
 
-// scroll animation
-import AOS from 'aos';
-import '../../node_modules/aos/dist/aos.css';
-AOS.init({
-  once: true
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 1000,
+  delay: 50,
+  //     reset: true
 });
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', {
+  delay: 400,
+});
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+
+const redirectTo = (url = "", target = "_blank") => {
+  if (!url.includes("https")) {
+    url = "https://" + url;
+  }
+
+  window.open(url, target);
+}
+
+const navLinkOnClick = (e) => {
+  if (!e.target.classList.contains("nav__link")) return;
+
+  document.querySelectorAll(".nav__link").forEach((x) => {
+    if (!x.classList.contains("active")) return;
+
+    x.classList.remove("active");
+  });
+  
+  e.target.classList.add("active");
+}
+
+document.addEventListener("click", (e) => {
+  navLinkOnClick(e);
+})
