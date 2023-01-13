c68a6327935ea4c447cd149ab27afdcae7bd030c
Daniel Sanches <danielsanches6301@gmail.com>
2023-01-13 14:01:02 -0300

    Entidade portfolio

---
+++ Portfolio.Domain/Entities/Portfolio.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Domain.Entities
+{
+    public class Portfolio : AbstractEntity
+    {
+        public string NomeCompleto { get; private set; }
+        public string Linkedin { get; private set; }
+        public string Facebook { get; private set; }
+        public string Instagram { get; private set; }
+        public string Youtube { get; private set; }
+        public string WhatsApp { get; private set; }
+        public string Email { get; private set; }
+        public ICollection<Projeto> Projetos { get; private set; }
+    }
+}
--- Portfolio.Domain/Entities/Projeto.cs
+++ Portfolio.Domain/Entities/Projeto.cs
@@ -6,6 +6,8 @@
         public string Descricao { get; private set; }
         public string Url { get; private set; }
         public string LinkGitHub { get; private set; }
+        public int PortfolioId { get; private set; }
+        public Portfolio Portfolio { get; private set; }
         public ICollection<ImagemProjeto> ImagemProjeto { get; private set; }
     }
 }
