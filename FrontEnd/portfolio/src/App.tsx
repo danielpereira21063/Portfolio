165da3cde71c6560ad3bd0040201c073401b2c9a
Daniel Sanches <danielsanches6301@gmail.com>
2023-01-15 22:34:34 -0300

    Ajustes em rotas e organização da pasta pages

--- FrontEnd/portfolio/src/App.tsx
+++ FrontEnd/portfolio/src/App.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 
 import Home from "./Pages/Home/Index";
-import Login from "./Pages/Account/Login/Index";
+import Login from "./Pages/Admin/Login/Index";
 import './App.css';
+import Admin from './Pages/Admin/Index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/admin' element={<Admin/>}></Route>
+        <Route path='/admin/login' element={<Login/>}></Route>
+        <Route path='*' element={<h1 style={{textAlign: 'center'}}>404 NOT FOUND!</h1>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ FrontEnd/portfolio/src/pages/Admin/Index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <div>Home page ADMIN</div>
+  )
+}
+
+export default Admin;
