5873f974fadc34a413ad6a46c1d1ac2d7e2c0ac6
Daniel Sanches <danielsanches6301@gmail.com>
2023-01-29 13:32:51 -0300

    novos ajustes

--- BackEnd/Portfolio.API/Program.cs
+++ BackEnd/Portfolio.API/Program.cs
@@ -21,6 +21,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.Urls.Add("https://127.0.0.1:7178");
 }
 else
 {
--- FrontEnd/portfolio/src/services/api/index.ts
+++ FrontEnd/portfolio/src/services/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const env = process.env.NODE_ENV || 'development';
 
-let API_URL = "https://portfolio-api.danielsanchesdev.com.br/api";
+let API_URL = "https://portfolio-api.dev.danielsanchesdev.com.br/api";
 
 if (env == "development") {
     // API_URL = "https://127.0.0.1:7178/api";
