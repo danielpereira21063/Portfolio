f337aa254109bd5572fe9256cd055b1aa3485821
Daniel Sanches <danielsanches6301@gmail.com>
2025-01-03 12:08:13 -0300

    ajustes

--- FrontEnd/html/src/scripts/index.js
+++ FrontEnd/html/src/scripts/index.js
@@ -1,74 +1,78 @@
-const buttonTop = document.getElementById('backToTopButton')
-const menuMobile = document.querySelector('#menu-hamburguer')
-
 window.sr = ScrollReveal({ reset: true });
 
-window.addEventListener('scroll', () => {
-    buttonTop.classList.add('show')
-    if (scrollY < 350) {
-        buttonTop.classList.remove('show')
-    }
-    else if (scrollY > 400) {
-        menuMobile.checked = false;
-    }
-})
+const buttonTop = document.querySelector('.button-top'); // Certifique-se de que essa classe existe
+const menuMobile = document.querySelector('.menu-mobile'); // Certifique-se de que essa classe existe
 
-buttonTop.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-})
+if (buttonTop && menuMobile) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 350) {
+            buttonTop.classList.add('show');
+        } else {
+            buttonTop.classList.remove('show');
+        }
 
+        if (window.scrollY > 400) {
+            menuMobile.checked = false;
+        }
+    });
 
+    buttonTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+// Configuração das animações com ScrollReveal
 sr.reveal('.texto-home', {
     duration: 1500,
     origin: 'top',
-    distance: '100px',
+    distance: '60px',
 });
-
 
 sr.reveal('.img-box', {
     duration: 1200,
     origin: 'left',
-    distance: '100px',
+    distance: '60px',
 });
 
 sr.reveal('.imagem-sobre-mim', {
     origin: 'bottom',
-    distance: '100px',
+    distance: '60px',
     duration: 1200
 });
 
 sr.reveal('.informacoes-sobre-mim', {
     origin: 'bottom',
-    distance: '100px',
+    distance: '60px',
     duration: 1200
 });
 
 sr.reveal('.section-title', {
     origin: 'top',
-    distance: '100px',
+    distance: '60px',
     duration: 1200
 });
 
 sr.reveal('.container-habilidades', {
     origin: 'bottom',
-    distance: '100px',
+    distance: '60px',
     duration: 1200
 });
 
 sr.reveal('.container-projetos', {
     origin: 'bottom',
-    distance: '100px',
+    distance: '60px',
     duration: 1200
 });
 
 sr.reveal('.cards-wrapper', {
     origin: 'top',
-    distance: '100px',
+    distance: '60px',
     duration: 0
 });
+
 
 const irPara = (url) => {
     window.open("https://" + url, '_blank');
